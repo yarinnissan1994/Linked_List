4b150ebe62b86b2ea19375b429068b7ba4d9a576
yarinnissan1994 <111521668+yarinnissan1994@users.noreply.github.com>
2022-08-30 00:59:29 +0300

    update

--- CW.c
+++ CW.c
@@ -79,12 +79,14 @@ void id()
 			printf("the number %d located in struct num %d.\n\n", id, count);
 			count++;
 		}
+	}
 }
 void SaveToFile(int size)
 {
 	struct Value
 	{
 		int Val;
+		struct Value* next;
 	};
 	struct Item* currentI = head;
 	struct Item* prevI;
@@ -98,6 +100,7 @@ void SaveToFile(int size)
 		prevI = currentI;
 		currentI = currentI->next;
 		currentV->Val = prevI->val;
+		currentV->next = prevI->next;
 		currentV++;
 	}
 	currentV = origin;
@@ -126,70 +129,103 @@ int structCount()
 	}
 	return count;
 }
+//void NewBeginnings()
+//{
+//	struct Copy
+//	{
+//		int val;
+//		struct Copy* next;
+//	};
+//	struct Copy* head = NULL;
+//	struct Copy* currentI;
+//	struct Copy* prevI = NULL;
+//	int ind = 1;
+//	FILE* f = fopen("hm7.bin", "r");
+//	while (ind)
+//	{
+//		currentI = malloc(sizeof(struct Copy));
+//		if (head == NULL)
+//		{
+//			head = currentI;
+//		}
+//		else
+//		{
+//			prevI->next = currentI;
+//		}
+//		int read = fread(currentI, sizeof(struct Copy), 1, f);
+//		currentI->next = NULL;
+//		prevI = currentI;
+//		if (read == 0)
+//		{
+//			ind = 0;
+//			break;
+//		}
+//	}
+//	fclose(f);
+//}
 
 int main()
 {
+	struct Item* currentI;
+	struct Item* prevI = NULL;
+	int userResponse;
+	int getIn = 1;
+	printf("Type 1 for Adding value\nType 2 for Print all List\nType 3 for Exit\n");
+	scanf("%d", &userResponse);
+	while (getIn != 100)
 	{
-		struct Item* currentI;
-		struct Item* prevI = NULL;
-		int userResponse;
-		int getIn = 1;
-		printf("Type 1 for Adding value\nType 2 for Print all List\nType 3 for Exit\n");
-		scanf("%d", &userResponse);
-		while (getIn != 100)
-		{
-			if (userResponse == 1)
-				{
-				currentI = malloc(sizeof(struct Item));
-				if (head == NULL)
-				{
-					head = currentI;
-				}
-				else
-				{
-					prevI->next = currentI;
-				}
-				printf("plaese enter a valeu:");
-				scanf("%d", &currentI->val);
-				getIn = currentI->val;
-				currentI->next = NULL;
-				prevI = currentI;
-				if (getIn != 100)
-				{
-				printf("\nType 1 for Adding value\nType 2 for Print all List\nType 3 for Exit\n");
-				scanf("%d", &userResponse);
-				}
-			}
-			else if (userResponse == 2)
+		if (userResponse == 1)
 			{
-				printLinkedList();
-				printf("\nType 1 for Adding value\nType 2 for Print all List\nType 3 for Exit\n");
-				scanf("%d", &userResponse);
-			}
-			else if (userResponse == 3)
+			currentI = malloc(sizeof(struct Item));
+			if (head == NULL)
 			{
-				break;
+				head = currentI;
 			}
 			else
 			{
-				printf("invalid number. please try agian:");
-				scanf("%d", &userResponse);
+				prevI->next = currentI;
+			}
+			printf("plaese enter a valeu:");
+			scanf("%d", &currentI->val);
+			getIn = currentI->val;
+			currentI->next = NULL;
+			prevI = currentI;
+			if (getIn != 100)
+			{
+			printf("\nType 1 for Adding value\nType 2 for Print all List\nType 3 for Exit\n");
+			scanf("%d", &userResponse);
 			}
 		}
-		find44();
-		id();
-		printf("save to file? 1 = yes | 0 = no:");
-		scanf("%d", &userResponse);
-		if (userResponse == 1)
+		else if (userResponse == 2)
 		{
-			SaveToFile(structCount());
-			printf("saved\n\n");
+			printLinkedList();
+			printf("\nType 1 for Adding value\nType 2 for Print all List\nType 3 for Exit\n");
+			scanf("%d", &userResponse);
+		}
+		else if (userResponse == 3)
+		{
+			break;
 		}
 		else
 		{
-			printf("skipped saving\n\n");
+			printf("invalid number. please try agian:");
+			scanf("%d", &userResponse);
 		}
-		freeLinkedList();
-		return 0;
 	}
+	find44();
+	id();
+	printf("save to file? 1 = yes | 0 = no:");
+	scanf("%d", &userResponse);
+	if (userResponse == 1)
+	{
+		SaveToFile(structCount());
+		printf("saved\n\n");
+	}
+	else
+	{
+		printf("skipped saving\n\n");
+	}
+	freeLinkedList();
+	//NewBeginnings();
+	return 0;
 }
